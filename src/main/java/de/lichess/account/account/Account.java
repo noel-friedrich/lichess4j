e5980bdcf95a324b9340eb4b36fca7df191b5fd5
KlSchlitzohr <lasisa.kurz@gmx.de>
2021-08-02 15:23:26 +0200

    Datenkapselung

--- src/main/java/de/lichess/account/account/Account.java
+++ src/main/java/de/lichess/account/account/Account.java
@@ -5,32 +5,32 @@ import lombok.Getter;
 @Getter
 public class Account {
 
-    public Perfs perfs;
-    public Count count;
-    public Profile profile;
-    public PlayTime playTime;
+    private Perfs perfs;
+    private Count count;
+    private Profile profile;
+    private PlayTime playTime;
 
-    public String id;
-    public String url;
-    public String title;
-    public String playing;
-    public String language;
-    public String username;
+    private String id;
+    private String url;
+    private String title;
+    private String playing;
+    private String language;
+    private String username;
 
-    public int nbFollowing;
-    public int nbFollowers;
-    public int completionRate;
+    private int nbFollowing;
+    private int nbFollowers;
+    private int completionRate;
 
-    public long seenAt;
-    public long createdAt;
+    private long seenAt;
+    private long createdAt;
 
-    public boolean patron;
-    public boolean online;
-    public boolean blocking;
-    public boolean disabled;
-    public boolean following;
-    public boolean streaming;
-    public boolean followable;
-    public boolean followsYou;
-    public boolean tosViolation;
+    private boolean patron;
+    private boolean online;
+    private boolean blocking;
+    private boolean disabled;
+    private boolean following;
+    private boolean streaming;
+    private boolean followable;
+    private boolean followsYou;
+    private boolean tosViolation;
 }
